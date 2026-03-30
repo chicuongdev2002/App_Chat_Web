4ce7cac65f5f9559d59898c54f288ff12a71b313
Nguyễn Chí Cường <124854803+chicuongdev2002@users.noreply.github.com>
2026-03-30 11:40:21 +0700

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyC7EHNEXx70w-Y95s2dZW6rR6diJDkL99M",
-  authDomain: "zalo-chats-10f1e.firebaseapp.com",
-  projectId: "zalo-chats-10f1e",
-  storageBucket: "zalo-chats-10f1e.appspot.com",
-  messagingSenderId: "1071228614114",
-  appId: "1:1071228614114:web:d237cd487ccf81952f35bb",
+  apiKey: "AIzaSyBGuW4k-UKU6_TReJw8FNItsvjb2Y77TbQ",
+  authDomain: "sold-chat.firebaseapp.com",
+  projectId: "sold-chat",
+  storageBucket: "sold-chat.firebasestorage.app",
+  messagingSenderId: "815204891348",
+  appId: "1:815204891348:web:76da2d51db6e0a9d659941",
+  measurementId: "G-K90HG4LF9X"
 };
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
